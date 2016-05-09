fdd12af9e0dc89e2ed5989eaa8d8c3d1124d8d79
antony-wilson <antony.wilson@stfc.ac.uk>
2016-05-09 09:20:32 +0100

    first commit

---
+++ cci_tagger/setup.py
@@ -0,0 +1,32 @@
+import os
+
+from setuptools import setup
+
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='cci_tagger',
+    version='0.0.1',
+    author=u'Antony Wilson',
+    author_email='antony.wilson@stfc.ac.uk',
+    include_package_data=True,
+    url='http://stfc.ac.uk/',
+    license='BSD licence',
+
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    # Adds dependencies
+    install_requires=[
+        'SPARQLWrapper==1.7.6',
+        'netCDF4==1.0.7',
+    ],
+)
+
